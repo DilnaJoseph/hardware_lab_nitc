dc82ca40eedb61c9db2458371bbe342cd6892de8
DilnaJoseph <dilnamjoseph@gmail.com>
2025-08-03 14:15:11 +0530

    full_adder.v

---
+++ practice/full_adder/full_adder.v
@@ -0,0 +1,15 @@
+module full_adder(input a,b,cin,output sum,cout);
+
+// sum = a xor b xor cin
+// cout = a.b + cin.(a xor b)
+
+wire xor_1,sum_1,sum_2;
+
+xor(xor_1,a,b);
+xor(sum,xor_1,cin);
+
+and(sum_1,a,b);
+and(sum_2,cin,xor_1);
+or(cout,sum_1,sum_2);
+
+endmodule
